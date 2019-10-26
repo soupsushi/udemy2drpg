a05c6f52d424e5d2f322e49bf5afdd748683e95d
soupsushi <soupsushi@users.noreply.github.com>
2019-10-26 14:14:53 +0900

    Section 1-4

---
+++ Udemy2dgame/Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Rigidbody2D theRB;
+    public float moveSpeed;
+    public Animator myAni;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+
+        myAni.SetFloat("moveX", theRB.velocity.x);
+        myAni.SetFloat("moveY", theRB.velocity.y); 
+
+        if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 ||
+           Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
+        {
+            myAni.SetFloat("lastMoveX", Input.GetAxisRaw("Horizontal"));
+            myAni.SetFloat("lastMoveY", Input.GetAxisRaw("Vertical"));
+        }
+
+    }
+}
